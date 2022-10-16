5eb8a39b3b02882ca5da4337dec3ac25ad0bf3d6
Bradford Johnson <bradford.lewis.johnson@outlook.com>
2022-10-16 11:31:05 -0400

    tg-percent-edit updated

--- adhoc/tg-percent-edit.r
+++ adhoc/tg-percent-edit.r
@@ -12,14 +12,26 @@ tg_df <- read_excel("tg-data.xlsx", "tg-data") %>%
 tg_df$date <- excel_numeric_to_date(tg_df$date)
 
 # [Apr 1st - Oct 14 2022] - percent edit plot
-tg_df %>%
-  ggplot(aes(x = date, y = percent_edit)) +
-  geom_area() +
-  geom_smooth(method = "lm") +
-  scale_y_continuous(labels = scales::percent_format(accuracy = 0.01)) +
-  labs(title = "Game Health",
-        subtitle = "Ball Edits",
-        x = "",
-        y = "Ball Edit %") +
-  theme_classic()
-#test
+  tg_df %>%
+    ggplot(aes(x = date, y = percent_edit)) +
+    geom_line() +
+    geom_smooth(method = "lm", se = FALSE, color = "red") +
+    scale_y_continuous(labels = scales::percent_format(accuracy = 0.01)) +
+    labs(title = "Ball Edit % Trends",
+      x = "",
+      y = "Ball Edit %") +
+    theme_classic()
+
+# average ball edit percent - month
+  tg_df %>%
+    mutate(month = month(date, label = TRUE, abbr = TRUE)) %>%
+    group_by(month) %>%
+    summarise(avg_edit_percent = mean(percent_edit)) %>%
+    ggplot(aes(x = month, y = avg_edit_percent, fill = avg_edit_percent)) +
+    geom_col() +
+    scale_y_continuous(labels = scales::percent_format(accuracy = 0.01)) +
+    labs(title = "Monthly Average: Ball Edit %",
+         x = "",
+         y = "") +
+    theme_classic() +
+    theme(legend.position = "none")
